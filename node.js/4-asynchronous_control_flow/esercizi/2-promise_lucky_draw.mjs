4bcbbbd8a9f8cfbaca5f4da694dec042236df595
valentina Li <valentinalidev@gmail.com>
2024-04-22 17:56:06 +0200

    modified exercise

--- node.js/4-asynchronous_control_flow/esercizi/2-promise_lucky_draw.mjs
+++ node.js/4-asynchronous_control_flow/esercizi/2-promise_lucky_draw.mjs
@@ -2,8 +2,6 @@
 // The `luckyDraw` function returns a promise. Create a promise chain where the function is called for for each of the players: Joe, Caroline and Sabrina
 // Log out the resolved value for each promise and handle any promise rejections in the chain.
 
-
-
 function luckyDraw(player) {
     return new Promise((resolve, reject) => {
         const win = Boolean(Math.round(Math.random()));
