aded096eb8db5c2c6db4cf6d28e5642faa9c6443
valentina Li <valentinalidev@gmail.com>
2024-04-22 17:56:00 +0200

    modified exercise

--- node.js/4-asynchronous_control_flow/esercizi/3-await_lucky_draw.mjs
+++ node.js/4-asynchronous_control_flow/esercizi/3-await_lucky_draw.mjs
@@ -1,5 +1,4 @@
 // Create agetResults function that uses async and await. Inside of the function, call the luckyDraw function for each of the players: Tina, Jorge, Julien
-
 // Log out the resolved value for each promise and handle any promise rejections.
 
 function luckyDraw(player) {
